2555324573bec4b9129bf8ae090d8e9bcfec0afd
pepperonirollz <cfitzpatrick616@gmail.com>
2024-05-01 21:46:30 -0400

    initial CRA

---
+++ src/components/login.tsx
@@ -0,0 +1,14 @@
+import TextField from "@mui/material/TextField";
+
+const Login = () => {
+  return (
+    <>
+      <h1>Welcome to HandHistory.com</h1>
+      <form>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </form>
+    </>
+  );
+};
+
+export default Login;
